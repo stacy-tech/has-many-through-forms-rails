94dfd04075d7ff1ac9f69c3f5647e5d0c36db628
stacy <stacydn23@gmail.com>
2021-03-26 22:51:43 -0400

    Done

--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,4 +1,10 @@
 class PostCategory < ActiveRecord::Base
   belongs_to :post
   belongs_to :category
+
+  private
+
+  def post_params
+    params.reequire(:post).permit(;title, :content, category_ids:[])
+  end
 end
